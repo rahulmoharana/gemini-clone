d1de47e1d6cbafa721a78117445adb285ed3b596
Rahul Moharana <123356880+rahulmoharana@users.noreply.github.com>
2025-07-10 17:36:36 +0530

    Update SideBar.jsx

--- src/components/Sidebar/SideBar.jsx
+++ src/components/Sidebar/SideBar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react";
-import "./SideBar.css";
+import "src/components/Sidebar/Sidebar.css";
 import { assets } from "../../assets/assets";
 import { Context } from "../../context/Context";
 const SideBar = () => {
